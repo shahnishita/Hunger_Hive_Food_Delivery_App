c052b4b8bab91af737e5621e575b1bbefc90c533
shahnishita <114091288+shahnishita@users.noreply.github.com>
2025-06-09 16:20:56 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StoreContextProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <StoreContextProvider>
         <App />
-      </BrowserRouter>
-    </StoreContextProvider>
+      </StoreContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
